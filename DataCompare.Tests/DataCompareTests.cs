0f9be4a5a23eb796a1e10eb6b2732e8b585fe776
michal-ciechan <lndcobra@hotmail.com>
2016-04-29 20:07:06 +0100

    Initial .NET Solution

---
+++ DataCompare.Tests/DataCompareTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using MoqqerNamespace;
+using NUnit.Framework;
+
+namespace DataCompare.Tests
+{
+    [TestFixture]
+    public class DataCompareTests
+    {
+        private Moqqer _moq;
+        private DataComparer _subject;
+
+        [SetUp]
+        public void A_TestSetup()
+        {
+            _moq = new Moqqer();
+            _subject = _moq.Create<DataComparer>();
+        }
+
+        [Test]
+        public void Compare_SameArrays_SameTrue()
+        {
+            var array1 = new []
+            {
+                new []{ "Key", "Value"},
+                new []{ "1", "Test"},
+            };
+
+            var array2 = new []
+            {
+                new []{ "Key", "Value"},
+                new []{ "1", "Test"},
+            };
+
+            var res = _subject.Compare(array1, array2);
+
+            res.Same.Should().BeTrue();
+        }
+    }
+}
---
+++ DataCompare/DataComparer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DataCompare
+{
+    public class DataComparer
+    {
+        public CompareResult Compare(IReadOnlyList<IList<string>> array1, IReadOnlyList<IList<string>> array2)
+        {
+            var res = new CompareResult
+            {
+                Same = true
+            };
+
+            return res;
+        }
+    }
+
+    public class CompareResult
+    {
+        public bool Same { get; set; }
+        public IReadOnlyList<IReadOnlyList<string>> Additional { get; set; }
+        public IReadOnlyList<IReadOnlyList<string>> Missing { get; set; }
+    }
+
+}
